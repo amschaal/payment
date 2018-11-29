ce0a0ce82d844561c3ad84e295b83364be7efb34
Adam Schaal <amschaal@ucdavis.edu>
2018-11-28 16:44:24 -0800

    add more filters

--- payment/api/views.py
+++ payment/api/views.py
@@ -5,7 +5,7 @@ from payment.models import Payment
 
 class PaymentViewset(viewsets.ReadOnlyModelViewSet):
     serializer_class = PaymentSerializer
-    filter_fields = {'id':[ 'icontains'],'order_id':[ 'icontains'],'description':['icontains'],'status':['exact'],'disabled':['exact'],'created':['gte','lte'],'paid_at':['gte','lte']}
+    filter_fields = {'id':[ 'icontains'],'order_id':[ 'icontains'],'description':['icontains'],'status':['exact'],'disabled':['exact'],'created':['gte','lte'],'paid_at':['gte','lte'],'status':['exact']}
 #     search_fields = ('title',)
     ordering_fields = ('created','status','amount','disabled','paid_at','order_id')
 #     permission_classes = (EventPermission,)
