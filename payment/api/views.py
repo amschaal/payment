7422d3c09305b63e6c7b462bf408fb9cee40e2e6
adam <amschaal@gmail.com>
2018-12-04 12:35:21 -0800

    Add filter for archived, update interface

--- payment/api/views.py
+++ payment/api/views.py
@@ -9,7 +9,7 @@ from rest_framework.permissions import IsAdminUser
 
 class PaymentViewset(viewsets.ReadOnlyModelViewSet):
     serializer_class = PaymentSerializer
-    filter_fields = {'id':[ 'icontains'],'order_id':[ 'icontains'],'description':['icontains'],'status':['exact'],'disabled':['exact'],'created':['gte','lte'],'paid_at':['gte','lte'],'status':['exact']}
+    filter_fields = {'id':[ 'icontains'],'order_id':[ 'icontains'],'description':['icontains'],'status':['exact'],'disabled':['exact'],'archived':['exact'],'created':['gte','lte'],'paid_at':['gte','lte'],'status':['exact']}
 #     search_fields = ('title',)
     ordering_fields = ('created','status','amount','disabled','paid_at','order_id')
     permission_classes = (IsAdminUser,)
